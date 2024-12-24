3fb68301e524ecc129e75e4e2a92064925db0fa4
Bao Khang <kngo3687@gmail.com>
2024-12-24 16:11:22 +0700

    fix a little

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -40,7 +40,7 @@
 		</div>
 	{:else}
 		<h1 class="text-4xl font-semibold text-center">Cảm ơn bạn đã gửi!</h1>
-		<h3>Hãy đợi mình trả lời nhé =))</h3>
+		<h3>Hãy đợi mình trả lời nhé (hoac ko) =))</h3>
 		<img class="h-72" src="/src/lib/pic.jpeg" alt="" />
 		<button
 			class="text-white w-72 bg-gradient-to-r transition-all duration-300 from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
